d8b09bc779e93d5a7af79cf1512a3356e5c560ed
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-05-19 23:14:59 +0600

    bootstrap and react-router is installed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import { getNodeText } from '@testing-library/dom';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
