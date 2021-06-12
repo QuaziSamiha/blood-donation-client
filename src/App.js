bbe70908d40846b5f5f572357779c03f6d250a59
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-06-12 09:28:20 +0600

    navbar task almost done

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className='app'>
-      <Header/>
       <Router>
+      <Header/>
         <Switch>
           <Route path='/home'>
             <Home />
